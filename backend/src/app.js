274355ff48b5102a7cdd8e39065264592c2638a6
Matteo <matteo.lalicata00@gmail.com>
2024-01-12 18:08:34 +0100

    Add cors to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,34 +1,41 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const stationRouter = require('./routes/station');
-const userRouter = require('./routes/user');
-const authRouter = require('./routes/auth');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const stationRouter = require("./routes/station");
+const userRouter = require("./routes/user");
+const authRouter = require("./routes/auth");
+const cors = require("cors");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
 app.use(express.json());
 
-app.use('/station', stationRouter);
-app.use('/user', userRouter);
-app.use('/auth', authRouter);
+app.use("/station", stationRouter);
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// send the error as json
-	res.status(err.status || 500);
-	res.json({ error: err.message });
+  // send the error as json
+  res.status(err.status || 500);
+  res.json({ error: err.message });
 });
 
 // start node server

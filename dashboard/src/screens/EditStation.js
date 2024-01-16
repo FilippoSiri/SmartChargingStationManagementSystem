3a8b1e4cfd5bc5c4347559230a55d31492fadfd6
Matteo <matteo.lalicata00@gmail.com>
2024-01-16 23:09:03 +0100

    Add logic to update station info

--- dashboard/src/screens/EditStation.js
+++ dashboard/src/screens/EditStation.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from "react";
 import {
   Box,
   Button,
+  Checkbox,
   Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Grid,
   Input,
+  Slide,
   TextField,
   Typography,
 } from "@mui/material";
@@ -12,10 +19,16 @@ import { useParams } from "react-router-dom";
 
 import axios from "axios";
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 const EditStation = () => {
   const { id } = useParams();
   const dismissedSelection = ["true", "false"];
+  const [open, setOpen] = useState(false);
   const [stationInfo, setStationinfo] = useState({});
+  const [dialogMessage, setDialogMessage] = useState("");
 
   useEffect(() => {
     const getStationInfo = async () => {
@@ -32,6 +45,44 @@ const EditStation = () => {
 
     getStationInfo();
   }, []);
+
+  const handleChangeStationInfo = (e) => {
+    let fieldToUpdate = e.target.getAttribute("field");
+    let newValue = e.target.value;
+
+    if (fieldToUpdate === "dismissed")
+      newValue = newValue === "true" ? true : false;
+
+    setStationinfo({ ...stationInfo, [fieldToUpdate]: newValue });
+  };
+
+  const handleSaveClick = async () => {
+    const res = await axios.patch(
+      `http://localhost:${process.env.REACT_APP_API_PORT}/station`,
+      {
+        id: stationInfo.id,
+        name: stationInfo.name,
+        lat: stationInfo.lat,
+        lon: stationInfo.lon,
+        price: stationInfo.price,
+        power: stationInfo.power,
+        dismissed: stationInfo.dismissed,
+        last_heartbeat: stationInfo.last_heartbeat,
+        notes: stationInfo.notes,
+      }
+    );
+
+    if (res.status === 201) {
+      setDialogMessage("Station updated successfully");
+    } else {
+      setDialogMessage("Error updating station");
+    }
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Container xs={"xl"}>
@@ -53,14 +104,19 @@ const EditStation = () => {
             <input
               className="input-edit-station"
               placeholder={stationInfo.name}
+              field="name"
+              onChange={handleChangeStationInfo}
             />
           </Grid>
 
           <Grid item xs={4}>
             <Typography variant="body1">Power</Typography>
             <input
+              type="text"
+              field="power"
               className="input-edit-station"
               placeholder={stationInfo.power}
+              onChange={handleChangeStationInfo}
             />
           </Grid>
 
@@ -68,20 +124,23 @@ const EditStation = () => {
             <Typography variant="body1">Price (€/kWh)</Typography>
             <input
               type="text"
+              field="price"
               className="input-edit-station"
               placeholder={stationInfo.price}
+              onChange={handleChangeStationInfo}
             />
           </Grid>
 
           <Grid item xs={4}>
             <Typography variant="body1">Dismissed</Typography>
-            <select className="input-edit-station">
-              <option value="true" selected={stationInfo.dismissed}>
-                True
-              </option>
-              <option value="false" selected={stationInfo.dismissed}>
-                False
-              </option>
+            <select
+              field="dismissed"
+              className="input-edit-station"
+              value={stationInfo.dismissed}
+              onChange={handleChangeStationInfo}
+            >
+              <option value="true">True</option>
+              <option value="false">False</option>
             </select>
           </Grid>
 
@@ -90,6 +149,8 @@ const EditStation = () => {
             <input
               className="input-edit-station"
               placeholder={stationInfo.lon}
+              field="lon"
+              onChange={handleChangeStationInfo}
             />
           </Grid>
 
@@ -98,6 +159,19 @@ const EditStation = () => {
             <input
               className="input-edit-station"
               placeholder={stationInfo.lat}
+              field="lat"
+              onChange={handleChangeStationInfo}
+            />
+          </Grid>
+
+          <Grid item xs={4}>
+            <Typography variant="body1">Last heartbeat</Typography>
+            <input
+              className="input-edit-station"
+              type="text"
+              value={new Date(stationInfo.last_heartbeat).toLocaleString()}
+              disabled
+              style={{ cursor: "not-allowed" }}
             />
           </Grid>
 
@@ -107,6 +181,8 @@ const EditStation = () => {
               className="input-edit-station"
               placeholder={stationInfo.notes}
               style={{ height: "100px", width: "100%" }}
+              field="notes"
+              onChange={handleChangeStationInfo}
             />
           </Grid>
 
@@ -117,7 +193,11 @@ const EditStation = () => {
               columns={{ xs: 4, sm: 8, md: 12 }}
             >
               <Grid item xs={1}>
-                <Button variant="contained" color="success">
+                <Button
+                  onClick={handleSaveClick}
+                  variant="contained"
+                  color="success"
+                >
                   Save
                 </Button>
               </Grid>
@@ -130,6 +210,23 @@ const EditStation = () => {
           </Grid>
         </Grid>
       </Box>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Update request"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            {dialogMessage}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>OK</Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 };

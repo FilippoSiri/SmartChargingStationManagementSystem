b1c34bda39f394336a678e7d4de41cb5873a8cc9
Matteo <matteo.lalicata00@gmail.com>
2024-01-17 18:29:49 +0100

    Add verifyTokenAdmin

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,19 +1,38 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 async function verifyToken(req, res, next) {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ error: 'Access denied' });
+    const token = req.header("Authorization");
+    if (!token) return res.status(401).json({ error: "Access denied" });
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        const user = await User.getById(decoded.userId);        
-        if(!user || user.token_reset_time > new Date(decoded.iat * 1000)) 
-            return res.status(401).json({ error: 'Invalid token' });
+        const user = await User.getById(decoded.userId);
+        if (!user || user.token_reset_time > new Date(decoded.iat * 1000))
+            return res.status(401).json({ error: "Invalid token" });
         req.userId = decoded.userId;
         next();
     } catch (error) {
-        res.status(401).json({ error: 'Invalid token' });
+        res.status(401).json({ error: "Invalid token" });
     }
-};
+}
 
-module.exports = verifyToken;
+async function verifyTokenAdmin(req, res, next) {
+    const token = req.header("Authorization");
+    if (!token) return res.status(401).json({ error: "Access denied" });
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const user = await User.getById(decoded.userId);
+        if (
+            !user ||
+            !user.is_admin ||
+            user.token_reset_time > new Date(decoded.iat * 1000)
+        )
+            return res.status(401).json({ error: "Invalid token" });
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        res.status(401).json({ error: "Invalid token" });
+    }
+}
+
+module.exports = { verifyToken, verifyTokenAdmin };

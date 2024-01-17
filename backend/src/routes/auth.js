43bb45ba0c881eb1e4b459c6e869c43b3a6ed4f5
Matteo <matteo.lalicata00@gmail.com>
2024-01-17 18:30:46 +0100

    Change import verifyToken + prettier indentation

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,35 +1,47 @@
-'use strict';
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User'); 
+"use strict";
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 const router = express.Router();
-const verifyToken = require('../middleware/authMiddleware');
+const { verifyToken } = require("../middleware/authMiddleware");
 //TODO: Check status code
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
     console.log(req.body);
-    const newUser = new User(null, req.body.name, req.body.surname, req.body.email, req.body.password, 0, new Date());
+    const newUser = new User(
+        null,
+        req.body.name,
+        req.body.surname,
+        req.body.email,
+        req.body.password,
+        0,
+        new Date(),
+        false
+    );
     const addedUser = await newUser.save();
     if (addedUser !== null) {
         const token = generateAccessToken({ userId: addedUser.id });
-        res.status(201).json({token: token});
+        res.status(201).json({ token: token });
     } else {
-        res.status(500).json({ message: 'Error adding user' });
+        res.status(500).json({ message: "Error adding user" });
     }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
     console.log(req.body);
     const user = await User.login(req.body.email, req.body.password);
     if (user !== null) {
-        const token = generateAccessToken({ userId: user.id });
-        res.json({token: token});
+        const token = generateAccessToken({
+            userId: user.id,
+            isAdmin: user.is_admin,
+        });
+        res.json({ token: token });
     } else {
-        res.status(401).json({ message: 'Login failed' });
+        res.status(401).json({ message: "Login failed" });
     }
 });
 
-router.get('/validateToken', verifyToken, async (req, res) => {
-    res.json({ message: 'Valid token' });
+router.get("/validateToken", verifyToken, async (req, res) => {
+    res.json({ message: "Valid token" });
 });
 //TODO: Check expiration
 function generateAccessToken(data) {

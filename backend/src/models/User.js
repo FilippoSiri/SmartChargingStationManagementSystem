d2b5566bec99d332e4c252e54dba7a0564458434
Matteo <matteo.lalicata00@gmail.com>
2024-01-17 18:30:05 +0100

    Modify user data model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,10 +1,19 @@
-'use strict';
-const conn = require('../utils/db-connection');
-const bcrypt = require('bcrypt');
-const { getById } = require('./station');
+"use strict";
+const conn = require("../utils/db-connection");
+const bcrypt = require("bcrypt");
+const { getById } = require("./station");
 
 class User {
-    constructor(id, name, surname, email, password, balance, token_reset_time) {
+    constructor(
+        id,
+        name,
+        surname,
+        email,
+        password,
+        balance,
+        token_reset_time,
+        is_admin
+    ) {
         this.id = id;
         this.name = name;
         this.surname = surname;
@@ -12,51 +21,96 @@ class User {
         this.password = password;
         this.balance = balance;
         this.token_reset_time = token_reset_time;
+        this.is_admin = is_admin;
     }
 
     async save() {
         if (this.id) {
             // update existing user
-            const sql = 'UPDATE User SET name = ?, surname = ?, email = ?, password = ?, balance = ?, token_reset_time = ? WHERE id = ?';
-            const [rows, _] = await conn.query(sql, [this.name, this.surname, this.email, await bcrypt.hash(this.password, 10), this.balance, this.token_reset_time, this.id]);
-            if(rows.affectedRows == 0)
-                return null;
+            const sql =
+                "UPDATE User SET name = ?, surname = ?, email = ?, password = ?, balance = ?, token_reset_time = ?, is_admin = ? WHERE id = ?";
+            const [rows, _] = await conn.query(sql, [
+                this.name,
+                this.surname,
+                this.email,
+                await bcrypt.hash(this.password, 10),
+                this.balance,
+                this.token_reset_time,
+                this.is_admin,
+                this.id,
+            ]);
+            if (rows.affectedRows == 0) return null;
         } else {
             // insert new user
-            const sql = 'INSERT INTO User (name, surname, email, password, balance, token_reset_time) VALUES (?, ?, ?, ?, ?, ?)';
-            const [rows, _] = await conn.query(sql, [this.name, this.surname, this.email, await bcrypt.hash(this.password, 10), this.balance, this.token_reset_time]);
-            if(rows.affectedRows == 0)
-                return null;
+            const sql =
+                "INSERT INTO User (name, surname, email, password, balance, token_reset_time, is_admin) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            const [rows, _] = await conn.query(sql, [
+                this.name,
+                this.surname,
+                this.email,
+                await bcrypt.hash(this.password, 10),
+                this.balance,
+                this.token_reset_time,
+                this.is_admin,
+            ]);
+            if (rows.affectedRows == 0) return null;
             this.id = rows.insertId;
         }
         return this;
     }
 
     static async login(email, password) {
-        const sql = 'SELECT * FROM User WHERE email = ?';
+        const sql = "SELECT * FROM User WHERE email = ?";
         const [rows, _] = await conn.query(sql, [email]);
-        if(rows.length == 0)
-            return null;
+        if (rows.length == 0) return null;
         const row = rows[0];
-        if(await bcrypt.compare(password, row.password))
-            return new User(row.id, row.name, row.surname, row.email, row.password, row.balance, row.token_reset_time);
-        else
-            return null;
+        if (await bcrypt.compare(password, row.password))
+            return new User(
+                row.id,
+                row.name,
+                row.surname,
+                row.email,
+                row.password,
+                row.balance,
+                row.token_reset_time,
+                row.is_admin
+            );
+        else return null;
     }
 
     static async getById(id) {
-        const sql = 'SELECT * FROM User WHERE id = ?';
+        const sql = "SELECT * FROM User WHERE id = ?";
         const [rows, _] = await conn.query(sql, [id]);
-        if(rows.length == 0)
-            return null;
+        if (rows.length == 0) return null;
         const row = rows[0];
-        return new User(row.id, row.name, row.surname, row.email, row.password, row.balance, row.token_reset_time);
+        return new User(
+            row.id,
+            row.name,
+            row.surname,
+            row.email,
+            row.password,
+            row.balance,
+            row.token_reset_time,
+            row.is_admin
+        );
     }
 
     static async getAll() {
-        const sql = 'SELECT * FROM User';
+        const sql = "SELECT * FROM User";
         const [rows, _] = await conn.query(sql);
-        return rows.map(row => new User(row.id, row.name, row.surname, row.email, row.password, row.balance, row.token_reset_time));
+        return rows.map(
+            (row) =>
+                new User(
+                    row.id,
+                    row.name,
+                    row.surname,
+                    row.email,
+                    row.password,
+                    row.balance,
+                    row.token_reset_time,
+                    row.is_admin
+                )
+        );
     }
 }
 

d287ae4bd82c18e0403b3895e4a41f92d8f35be8
Wh0ll <73947352+Wh0ll@users.noreply.github.com>
2024-01-19 18:42:59 +0100

    Commented RPCServer before further testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,11 @@ const express = require("express");
 const stationRouter = require("./routes/station");
 const userRouter = require("./routes/user");
 const authRouter = require("./routes/auth");
-const chargingStation = require("./routes/chargingstation");
+//const chargingStation = require("./routes/chargingstation");
 
 const cors = require("cors");
 var path = require('path');
-const { RPCServer, RPCClient } = require("ocpp-rpc");
+//const { RPCServer, RPCClient } = require("ocpp-rpc");
 
 const app = express();
 
@@ -22,22 +22,22 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-const allClients = new Map();
+/*const allClients = new Map();
 const httpServer = app.listen(process.env.RPC_PORT, "localhost");
 const rpcServer = new RPCServer();
-httpServer.on("upgrade", rpcServer.handleUpgrade);
+httpServer.on("upgrade", rpcServer.handleUpgrade);*/
 
-rpcServer.on("client", (client) => {
+/*rpcServer.on("client", (client) => {
     allClients.set(client.identity, client); 
     client.call("Say", `Hello, ${client.identity}!`);
-});
+});*/
 
 app.use(express.json());
 
 app.use("/station", stationRouter);
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
-app.use("/chargingstation", chargingStation);
+//pp.use("/chargingstation", chargingStation);
 
 
 // catch 404 and forward to error handler

e3bb281dbcdf07ddd8d312bcba58f438cb871d48
Wh0ll <73947352+Wh0ll@users.noreply.github.com>
2024-02-08 18:48:24 +0100

    Fix merge

---
+++ backend/src/services/RPCStationService.js
@@ -0,0 +1,65 @@
+class RPCStationService{
+    static stations = new Map();
+
+    static addStation(station) {
+        this.stations.set(station.identity, station);
+        console.log(this.stations);
+    }
+    
+    static async remoteStartTransaction(stationId){
+        try{
+            const response = await this.stations.get(stationId).call('RemoteStartTransaction', {
+                connectorId: 0,
+                idTag: "123456"
+            });
+            return response.status === 'Accepted';   
+        }catch(e){
+            console.log(e);
+            return false;
+        }
+    }
+    
+    static async remoteStopTransaction(stationId){
+        try{
+            const response = await this.stations.get(stationId).call('RemoteStopTransaction', {
+                transactionId: 1234
+            });
+            return response.status === 'Accepted';   
+        }catch(e){
+            console.log(e);
+            return false;
+        }
+        
+    }
+    
+    static async reserveNow(stationId){
+        try{
+            const response = await this.stations.get(stationId).call('ReserveNow', {
+                connectorId: 0,
+                expiryDate: new Date().toISOString(),
+                idTag: "1234",
+                parentIdTag: "1234",
+                reservationId: 1234
+            });
+            return response.status === 'Accepted';
+        }catch(e){
+            console.log(e);
+            return false;
+        }
+    }
+    
+    static async cancelReservation(stationId){
+        try{
+            const response = await this.stations.get(stationId).call('CancelReservation', {
+                reservationId: 1234
+            });
+            return response.status === 'Accepted';
+        }catch(e){
+            console.log(e);
+            return false;
+        }
+    }
+    
+}
+
+module.exports = RPCStationService;
